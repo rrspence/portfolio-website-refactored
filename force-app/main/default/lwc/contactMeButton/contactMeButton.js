e5d8ab3d1115d81fdb3ee853eb57f3c08375db77
rahim <rahim@rahims-MacBook-Pro.local>
2025-05-11 17:37:51 -0600

    Add all fields to contact form and prepare data structure for Apex

--- force-app/main/default/lwc/contactMeButton/contactMeButton.js
+++ force-app/main/default/lwc/contactMeButton/contactMeButton.js
@@ -2,12 +2,22 @@ import { LightningElement } from 'lwc';
 
 export default class ContactMeButton extends LightningElement {
     dialog;
+    firstname;
+    lastname;
+    company;
+    email;
+    description;
 
     renderedCallback() {
         if (!this.dialog) {
             this.dialog = this.template.querySelector('.contact-dialog');
+            this.firstname = this.template.querySelector('.firstname');
+            this.lastname = this.template.querySelector('.lastname');
+            this.company = this.template.querySelector('.company');
+            this.email = this.template.querySelector('.email');
+            this.description = this.template.querySelector('.description');
         }
-    }
+    }    
 
     showDialog() {
         this.dialog.showModal();
@@ -18,9 +28,18 @@ export default class ContactMeButton extends LightningElement {
     }
 
     handleSubmit(event) {
-        event.preventDefault(); // prevent page reload
-        const firstName = this.template.querySelector('.firstname').value;
-        console.log('First Name:', firstName);
+        event.preventDefault();
+    
+        const data = {
+            firstname: this.firstname.value,
+            lastname: this.lastname.value,
+            company: this.company.value,
+            email: this.email.value,
+            description: this.description.value
+        };
+    
+        console.log('Form submitted with:', data);
         this.closeDialog();
     }
+    
 }
