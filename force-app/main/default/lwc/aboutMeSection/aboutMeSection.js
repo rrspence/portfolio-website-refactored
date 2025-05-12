b443f2b3092905f438181c9bde510161f0af06a7
rahim <rahim@rahims-MacBook-Pro.local>
2025-05-11 22:16:05 -0600

    Add About Me section with responsive SLDS layout and image

---
+++ force-app/main/default/lwc/aboutMeSection/aboutMeSection.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import IMAGES from '@salesforce/resourceUrl/images';
+
+export default class AboutMeSection extends LightningElement {
+    aboutMeImageUrl = IMAGES + '/aboutme.jpg'; // Update filename as needed
+}
